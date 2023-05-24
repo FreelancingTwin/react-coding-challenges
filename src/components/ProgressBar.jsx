596a252845787b125626cff7f72f22ed03358478
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-05-24 16:57:04 +0530

    Integrated UI library PicoCSS, and progress bar optimization

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,11 +1,14 @@
 import {useState} from 'react'
 function ProgressBar() {
-    const [percentage, setPercentage] = useState(30);
+    const [percentage, setPercentage] = useState();
     return (
-    <div>
-        <h3>ProgressBar: Changes accordig to input value</h3>
+    <div className='container-fluid'>
+        <h3>ProgressBar: Fills accordig to input value</h3>
       <progress max="100" min="1" value={percentage}/>
-      <input type="number" name="number" id="number" max='100' min = '0' onChange={(e)=>{setPercentage(e.target.value)}} placeholder="30%"/>
+      <div className='grid'>
+      <label htmlFor="number">Input:</label>
+      <input type="number" name="number" id="number" max="100" min ='0' maxLength='2' onChange={(e)=>{setPercentage(e.target.value)}} placeholder="30%"/>
+      </div>
     </div>
   )
 }
